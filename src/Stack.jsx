b87edba6d7698d7152a1790fdf89f679583dbdd4
erpicoding <erik.pischke.bs@icloud.com>
2026-01-10 22:45:45 +0100

    add old stack

--- src/Stack.jsx
+++ src/Stack.jsx
@@ -2,6 +2,10 @@ import scssImg from "./assets/logos/scss.svg";
 import reactImg from "./assets/logos/react.png";
 import nodeImg from "./assets/logos/node.svg";
 
+import htmlImg from "./assets/logos/html.webp";
+import cssImg from "./assets/logos/css.svg";
+import jsImg from "./assets/logos/js.svg";
+
 import StackItem from "./StackItem";
 
 function Stack() {
@@ -12,6 +16,12 @@ function Stack() {
         <StackItem name="SCSS" img={scssImg} />
         <StackItem name="React" img={reactImg} />
         <StackItem name="NodeJS" img={nodeImg} />
+      </div>
+      <h2>Old Stack</h2>
+      <div className="stackRow">
+        <StackItem name="HTML" img={htmlImg} />
+        <StackItem name="CSS" img={cssImg} />
+        <StackItem name="JavaScript" img={jsImg} />
       </div>
     </section>
   );
