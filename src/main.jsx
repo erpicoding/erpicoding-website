c713f08aeca9ca8f907650bf30aa7f5e1771c1b6
erpicoding <erik.pischke.bs@icloud.com>
2026-01-16 09:07:54 +0100

    better button design

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./css/hero.css";
 import "./css/stack.css";
+import "./css/projects.css";
 import "./css/main.css";
 
 createRoot(document.getElementById("root")).render(
